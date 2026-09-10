9f393237944105dba578bc7251faf2e7b63904f5
Ginêz Carmona <ginez.carmona565@gmail.com>
2026-09-09 22:38:10 -0300

    try catch

--- backend/conexao.php
+++ backend/conexao.php
@@ -7,8 +7,12 @@ $usuario = 'root';
 $senha = '';
 
 try {
-    $pdo = new PDO("mysql:host={$host};port={$porta};dbname{$banco};charset=utf8mb4", $usuario, $senha, [ PDO::ATTR_ERRMODE])
+    $pdo = new PDO(
+        "mysql:host={$host};port={$porta};dbname{$banco};charset=utf8mb4", $usuario, $senha, 
+        [ 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FECT_MODE => PDO::FETCH ASSOC,
+        ]
+    );
 }
 ?>
-
-teste teste testeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
