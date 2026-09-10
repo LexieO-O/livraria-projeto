8e18ebb7b03fdbdbab17706a8b5dab5c8df925a7
51725-GIT <51725@fai.com.br>
2026-09-09 22:08:27 -0300

    Inserindo os dados de categoorias, autores e livros nas tabelas do banco de dados

--- database/script_criacao.sql
+++ database/script_criacao.sql
@@ -23,3 +23,21 @@ create table livros (
 	id_autor int foreign key references autores (id_autor),
 	id_categoria int foreign key references categorias (id_categoria)
 );
+
+insert into categorias (nome) values ('Romance');
+insert into categorias (nome) values ('Ficcao Cientifica');
+insert into categorias (nome) values ('Tecnologia');
+insert into categorias (nome) values ('Biografia');
+
+insert into autores (nome, nacionalidade) values ('Machado de Assis', 'Brasileira');
+insert into autores (nome, nacionalidade) values ('George Orwell', 'Britanica');
+insert into autores (nome, nacionalidade) values ('Robert C. Martin', 'Americana');
+
+insert into livros (titulo, ano_publicacao, isbn, quantidade_estoque, preco, id_autor, id_categoria)
+values ('Dom Casmurro', 1899, '9788525406958', 12, 29.90, 1, 1);
+
+insert into livros (titulo, ano_publicacao, isbn, quantidade_estoque, preco, id_autor, id_categoria)
+values ('1984', 1949, '9780451524935', 8, 39.90, 2, 2);
+
+insert into livros (titulo, ano_publicacao, isbn, quantidade_estoque, preco, id_autor, id_categoria)
+values ('Clean Code', 2008, '9780132350884', 5, 89.90, 3, 3);
