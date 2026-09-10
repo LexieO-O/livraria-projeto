fdca238ab1e98140d431941ef78abcf75d77a9b1
Ginêz Carmona <ginez.carmona565@gmail.com>
2026-09-09 22:34:21 -0300

    Conexão com banco de dados

--- backend/conexao.php
+++ backend/conexao.php
@@ -5,4 +5,8 @@ $host = 'localhost';
 $porta = '3306';
 $usuario = 'root';
 $senha = '';
+
+try {
+    $pdo = new PDO("mysql:host={$host};port={$porta};dbname{$banco};charset=utf8mb4", $usuario, $senha, [ PDO::ATTR_ERRMODE])
+}
 ?>
